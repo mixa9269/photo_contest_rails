3c5dc642781efd47307191225bb97e8a26068d58
Mikhail Kaminsky <kam.m.a@ya.ru>
2019-08-08 13:40:49 +0300

    Add sort to gallery

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,13 @@ class StaticPagesController < ApplicationController
   end
 
   def gallery
-    @photos = Photo.approved.page(params[:page]).per(5)
+    if params[:sort] == 'by_date'
+      @by_date = true
+      photos = Photo.approved.reorder(created_at: :desc)
+    else
+      @by_date = false
+      photos = Photo.approved.reorder(likes_count: :desc)
+    end
+    @photos = photos.page(params[:page]).per(5)
   end
 end
