5a28763b09d2cc638f7cfa91a2c19e4636df6d68
Mikhail Kaminsky <kam.m.a@ya.ru>
2019-08-25 12:02:38 +0300

    Refactoring photos_controller index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -30,18 +30,8 @@ class PhotosController < ApplicationController
 
   def index
     @search = params[:search] || ''
-    photos = Photo.approved.search(@search)
-    if params[:sort] == 'by_date'
-      @by_date = true
-      photos = photos.reorder(created_at: :desc)
-    elsif params[:sort] == 'by_comments'
-      @by_comments = true
-      photos = photos.reorder(comments_count: :desc)
-    else
-      @by_rating = true
-      photos = photos.reorder(likes_count: :desc)
-    end
-    @photos = photos.page(params[:page]).per(5)
+    @sort = params[:sort] || 'likes_count'
+    @photos = Photo.approved.search(@search).reorder("#{@sort} DESC").page(params[:page]).per(5)
   end
 
   def destroy
